6e4b6d8e8d6d5226e9cf3c3c38f50de7e2164a4a
daniel <daniel.gomez3@utp.ac.pa>
2023-12-22 13:18:03 -0500

    Update page layout and content

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,12 @@ export default function Home() {
       <div className="flex items-center justify-center h-screen bg-gradient px-24 p-16">
 
         <div className="w-full h-full  bg-white bg-opacity-20 shadow-lg rounded-md">
-          <div className="w-full h-full grid  grid-cols-2">
-            <div className="content flex justify-center align-middle text-center w-full h-full border">
-              <h1 className=" font-medium text-5xl ">Pokemon</h1>
+          <div className="w-full h-full grid  grid-cols-2 p-0 m-0  z-9">
+            <div className=" flex justify-center items-center gap-8 text-center w-full h-full p-0 m-0 border flex-col z-9 ">
+              <h1 className=" border m-0 p-0 font-bold text-9xl text-white">POKEBOLA</h1>
+              <h2 className=" w-64">Su nombre proviene de Pocket Ball, o bola de bolsillo, el cual se debe a su pequeño tamaño o bien del propio nombre de los Pokémon.</h2>
             </div>
-            <div>
+            <div className="z-10">
               <iframe
                 className="w-full h-full"
                 src="https://my.spline.design/untitled-2e3b0503a15ff598079cedab05bec980/"
